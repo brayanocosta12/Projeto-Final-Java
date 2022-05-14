758d813f753904db4ab65ff7930a147252bad689
MayaraNF <mnfigueiredo101@gmail.com>
2022-05-14 00:41:23 -0300

    [1] organização dos arquivos do projeto e novo arquivo para modelagem geral.

---
+++ petgree/src/main/java/com/senai/projetofinal/petgree/PetgreeApplication.java
@@ -0,0 +1,13 @@
+package com.senai.projetofinal.petgree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetgreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetgreeApplication.class, args);
+	}
+
+}
---
+++ petgree/src/test/java/com/senai/projetofinal/petgree/PetgreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.projetofinal.petgree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetgreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
